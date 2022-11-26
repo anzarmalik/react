2d139a91a3fdd1ab2b90e29f3e0b2c03a64ec695
Anzar Malik <anzarmalikcse@gmail.com>
2022-11-26 12:53:40 +0530

    first commit

---
+++ jsx/src/index.js
@@ -0,0 +1,11 @@
+
+// import react from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = () => {
+    return <div>hi i am abc</div>;
+}
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
