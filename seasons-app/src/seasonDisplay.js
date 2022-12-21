0b6a7fe54604b30c04bfd64947b0f62efc98cb11
Anzar Malik <anzarmalikcse@gmail.com>
2022-12-21 23:38:47 +0530

    added get Season data to get season based on location

--- seasons-app/src/seasonDisplay.js
+++ seasons-app/src/seasonDisplay.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+const getSeason = (lat, month)=>{
+    if(month > 2 || month <9){
+      return lat > 0 ? 'summer' : 'winter';
+    }else{
+      return lat > 0 ? 'winter' : 'summer';
+    }
+}
+
+
 const SeasonDisplay = (props) => {
     console.log("🚀 ~ file: seasonDisplay.js:4 ~ SeasonDisplay ~ props", props)
-    return (<div> Hi There i am in Latitude {props.detailObj.lat}
+    return (<div> Hi There i am in Latitude {props.detailObj.lat} and season is {getSeason(props.detailObj.lat,new Date().getMonth())}
         <br /> Error : {props.detailObj.error} </div>);
 }
 
