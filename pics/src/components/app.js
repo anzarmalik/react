df33b966608b01ce3975df273f77b4fc6abf78f5
Anzar Malik <anzarmalikcse@gmail.com>
2022-12-28 21:49:40 +0530

    added search bar component

--- pics/src/components/app.js
+++ pics/src/components/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import SearchBar from './searchBar';
 
 const App = () => {
-    return <div>App</div>
+    return (<div><SearchBar /></div>)
 };
 
 export default App;
---
+++ pics/src/components/searchBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    render() {
+        return (<div>
+            <form>
+                <input type='text' className='' />
+            </form>
+        </div>)
+    }
+
+}
+
+export default SearchBar
