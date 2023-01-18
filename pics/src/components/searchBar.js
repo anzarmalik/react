96f96688b794820b7c9c05b8d34dbb9c78c2bbb3
Anzar Malik <anzarmalikcse@gmail.com>
2023-01-18 21:44:44 +0530

    added semantic ui styling

--- pics/src/components/searchBar.js
+++ pics/src/components/searchBar.js
@@ -2,13 +2,13 @@ import React from "react";
 
 class SearchBar extends React.Component {
     render() {
-        return (<div>
-            <form>
-                <input type='text' className='' />
+        return (<div className='ui segment' >
+            <form className= 'ui form'>
+                <label >Image Search</label>
+                <input type='text' className='field' />
             </form>
         </div>)
     }
-
 }
 
 export default SearchBar
