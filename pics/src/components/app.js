13528922fca907ca2ea0655466bb3bd2d930e5c9
Anzar Malik <anzarmalikcse@gmail.com>
2023-02-10 00:20:13 +0530

    added unsplash api to get pics

--- pics/src/components/app.js
+++ pics/src/components/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import SearchBar from './searchBar';
-
+import axios from 'axios'
 // const App = () => {
 //     return (<div className= 'ui container'  style={{marginTop:'10px'}} ><SearchBar /></div>)
 // };
@@ -8,6 +8,13 @@ import SearchBar from './searchBar';
 class App extends React.Component {
     onSearchSubmit(term) {
         console.log('term :', term);
+        axios.get('https://api.unsplash.com/',{
+            params: {query: term},
+            headers: {
+                Authorization : 'Client-ID Cwo8L2PBpDWDs2CPIc6TRX9q0HLxk7s3sKV1gmakvDo'
+            }
+        });
+        
     }
     render() {
         return (
