3a41a4f6685e13440697e4ce409eb058b3457154
Anzar Malik <anzarmalikcse@gmail.com>
2023-02-11 01:08:34 +0530

    minor change pushed

--- pics/src/components/app.js
+++ pics/src/components/app.js
@@ -6,19 +6,25 @@ import axios from 'axios'
 // };
 
 class App extends React.Component {
-    onSearchSubmit(term) {
-        console.log('term :', term);
-        axios.get('https://api.unsplash.com/search/photos',{
+    state = {
+        images: []
+    }
+    onSearchSubmit = async(term)=> {
+        // console.log('term :', term);
+       const response = await  axios.get('https://api.unsplash.com/search/photos',{
             params: {query: term},
             headers: {
                 Authorization : 'Client-ID Cwo8L2PBpDWDs2CPIc6TRX9q0HLxk7s3sKV1gmakvDo'
             }
         });
-        
+        console.log(response.data.results)
+        this.setState({images : response.data.results});
     }
     render() {
         return (
-            <div className='ui container' style={{ marginTop: '10px' }} ><SearchBar searchSubmit={this.onSearchSubmit} /></div>
+            <div className='ui container' style={{ marginTop: '10px' }} ><SearchBar searchSubmit={this.onSearchSubmit} />
+            Found : {this.state.images.length}
+            </div>
         );
     }
 }
