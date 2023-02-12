0caaf05938516a1651e85fbd7508d22f9cca932f
Anzar Malik <anzarmalikcse@gmail.com>
2023-02-12 17:34:53 +0530

    added grd rows specific to image

--- pics/src/components/imageCard.js
+++ pics/src/components/imageCard.js
@@ -2,8 +2,27 @@ import '../imageList.css';
 import React from 'react'
 
 class ImageCard extends React.Component {
+
+    constructor(props){
+        super(props)
+
+        this.state={
+            span:0
+        }
+        this.imageRef =  React.createRef()
+    }
+
+    componentDidMount() {
+      this.imageRef.current.addEventListener('load', this.setSpans)
+    }
+
+    setSpans = ()=>{
+        const imageHeight = this.imageRef.current.clientHeight;
+        this.setState({span: Math.ceil(imageHeight/ 10) });
+    }
+
    render() {
-    return (<div><img alt={this.props.imageDetail.description} key={this.props.imageDetail.id} src={this.props.imageDetail.src} /></div>)
+    return (<div style={{gridRowEnd : `span ${this.state.span}`}} ><img  ref={this.imageRef} alt={this.props.imageDetail.description} key={this.props.imageDetail.id} src={this.props.imageDetail.src} /></div>)
    }
 }
 
