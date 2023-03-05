d75ec6e9fc7788dd9f04ada064654349caee26cc
Anzar Malik <anzarmalikcse@gmail.com>
2023-03-05 13:33:59 +0530

    onclick event added

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const Accordion = ({items})=>{
-    const itemsList = items.map(item => {
+
+    const itemClick = (index)=>{
+      console.log('item click index : ',index);
+    };
+
+    const itemsList = items.map((item,index) => {
         return (<React.Fragment key={item.title}>
-            <div className="title active">
+            <div className="title active"  onClick ={()=>{itemClick(index)}} >
              <i className="dropdown icon"></i>
              {item.title}
             </div>
