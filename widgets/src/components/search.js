7580348310395d2dfd7c8b3a7adf892ab9315092
Anzar Malik <anzarmalikcse@gmail.com>
2023-03-19 22:26:52 +0530

    using another state

--- widgets/src/components/search.js
+++ widgets/src/components/search.js
@@ -2,24 +2,26 @@ import React ,{ useState ,useEffect} from 'react';
 import axios from 'axios';
 
 const Search = ()=>{
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
+    const [results, setResults] = useState([]);
 
     useEffect(() =>{
       const search = async()=>{
-           await axios.get('https://en.wikipedia.org/w/api.php',{
+           const {data} = await axios.get('https://en.wikipedia.org/w/api.php',{
                params: {
                    action: 'query',
                    list:'search',
                    origin:'*',
                    format:'json',
                    srsearch:term,
-
                }
            });
+        setResults(data.query.search);
+        // console.log("🚀 ~ file: search.js:22 ~ search ~ setResults(data.query.search);:", data.query.search);
         };
-        search()
+        search();
         // console.log("🚀 ~ file: search.js:23 ~ useEffect ~ search:", search())
-    } )
+    },[term] )
 
     return (
         <div>
