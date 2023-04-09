bf6f41340e8f4e8e6dc875e1aad947d75b20a1b2
Anzar Malik <anzarmalikcse@gmail.com>
2023-04-09 14:30:48 +0530

    created google translate API

--- widgets/src/components/translate.js
+++ widgets/src/components/translate.js
@@ -1,35 +1,38 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Dropdown from './dropDown';
-
+import Convert from './convert'
 const options = [
     {
-        label:'Africi',
-        value:'af'
+        label: 'Africi',
+        value: 'af'
     },
     {
-        label:'Arabic',
-        value:'ar'
+        label: 'Arabic',
+        value: 'ar'
     },
     {
-        label:'Hindi',
-        value:'Hi'
+        label: 'Hindi',
+        value: 'Hi'
     }
 ]
 
-const Translate = ()=>{
-const [language, setLanguage] = useState(options[0])
-const [text, selectText] = useState()
-// AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
+const Translate = () => {
+    const [language, setLanguage] = useState(options[0])
+    const [text, selectText] = useState()
 
-return(
-    <div  className="ui form" >
-        <div className="field">
-            <label>Enter text</label>
-            <input  value={text}  onChange={(e)=>{ selectText(e.target.value)}} />
+    return (
+        <div className="ui form" >
+            <div className="field">
+                <label>Enter text</label>
+                <input value={text} onChange={(e) => { selectText(e.target.value) }} />
+            </div>
+            <Dropdown selected={language} setSelection={setLanguage} options={options} label={"language"} />
+            <div>
+                <h3 className="ui header">Output</h3>
+                <Convert text={text}  language={language} />
+            </div>
         </div>
-        <Dropdown  selected={language}  setSelection={setLanguage} options={options} label={"language"} />
-    </div>
-)
+    )
 }
 
 export default Translate
