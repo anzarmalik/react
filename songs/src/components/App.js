c2ea4535bcb58af4ec07881a652a3a919264522e
Anzar Malik <anzarmalikcse@gmail.com>
2023-04-30 05:49:19 +0530

    adding redux

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import SongList from "./songList";
 
-const App = ()=>{
-    return <div>App</div>
+const App = () => {
+    return (<div className="ui container grid" >
+        <div className="ui row" >
+            <div className="column eight wide" >
+                <SongList />
+            </div>
+        </div>
+    </div>)
 }
 
 export default App;
