69c8df1b1a1b44250b930a584b6ea97c039d26a3
Anzar Malik <anzarmalikcse@gmail.com>
2023-06-19 22:06:47 +0530

    created fetch user action

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -7,3 +7,11 @@ export const fetchPosts = () => async dispatch => {
         payload: response.data,
     });
 }
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceHolder.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+    return dispatch({
+        type: "FETCH_USER",
+        payload: response.data,
+    });
+}
