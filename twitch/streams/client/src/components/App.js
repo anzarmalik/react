507a8b307f759c41ae9d82049788ca9ef5b08610
Anzar Malik <anzarmalikcse@gmail.com>
2023-07-01 20:15:41 +0530

    working on twitch navigation

---
+++ twitch/streams/client/src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import StreamList from "../components/streams/StreamList";
+import StreamCreate from "../components/streams/StreamCreate";
+import StreamEdit from "../components/streams/StreamEdit";
+import StreamDelete from "../components/streams/StreamDelete";
+import StreamShow from "../components/streams/StreamShow";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<StreamList/>} />
+          <Route path="/streams/new" exact element={<StreamCreate/>} />
+          <Route path="/streams/edit" exact element={<StreamEdit/>} />
+          <Route path="/streams/delete" exact element={<StreamDelete/>} />
+          <Route path="/streams/show" exact element={<StreamShow/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
