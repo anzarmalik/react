0ac7cdffa912eb8ebc9e424b2311b3ae24960483
Anzar Malik <anzarmalikcse@gmail.com>
2023-07-03 11:37:19 +0530

    adding header

--- twitch/streams/client/src/components/App.js
+++ twitch/streams/client/src/components/App.js
@@ -5,17 +5,19 @@ import StreamCreate from "../components/streams/StreamCreate";
 import StreamEdit from "../components/streams/StreamEdit";
 import StreamDelete from "../components/streams/StreamDelete";
 import StreamShow from "../components/streams/StreamShow";
+import Header from "./Header"
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Header />
         <Routes>
-          <Route path="/" exact element={<StreamList/>} />
-          <Route path="/streams/new" exact element={<StreamCreate/>} />
-          <Route path="/streams/edit" exact element={<StreamEdit/>} />
-          <Route path="/streams/delete" exact element={<StreamDelete/>} />
-          <Route path="/streams/show" exact element={<StreamShow/>} />
+          <Route path="/" exact element={<StreamList />} />
+          <Route path="/streams/new" exact element={<StreamCreate />} />
+          <Route path="/streams/edit" exact element={<StreamEdit />} />
+          <Route path="/streams/delete" exact element={<StreamDelete />} />
+          <Route path="/streams/show" exact element={<StreamShow />} />
         </Routes>
       </BrowserRouter>
     </div>
