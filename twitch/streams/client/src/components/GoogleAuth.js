356affcb3694d7b1a686069bf11328fd622a5c9e
Anzar Malik <anzarmalikcse@gmail.com>
2023-07-18 23:40:11 +0530

    adding google oauth signIn out logic

--- twitch/streams/client/src/components/GoogleAuth.js
+++ twitch/streams/client/src/components/GoogleAuth.js
@@ -1,18 +1,35 @@
 import React from 'react';
 
 class GoogleAuth extends React.Component {
-    componentDidMount(){
-        window.gapi.load('client:auth2',()=>{
+    state = {isSignedIn : null};
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
-                clientId: '...',
-                scope:'email',
-                plugin_name:'App Name that you used in google developer console API'
+                clientId: '',
+                scope: 'email',
+                plugin_name: 'App Name that you used in google developer console API'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({
+                    isSignedIn: this.auth.isSignedIn.get()
+                });
             })
         })
     }
 
-    render(){
-        return <div>Google Auth</div>
+    oAuthButtonText() {
+        if (this.state.isSignedIn === null) {
+            return "status not known! "
+        } else if (this.state.isSignedIn) {
+            return "signed In! "
+        } else {
+            return "signed out"
+        }
+    }
+
+    render() {
+        return <div>Google Auth {this.oAuthButtonText()}</div>
     }
 }
 
