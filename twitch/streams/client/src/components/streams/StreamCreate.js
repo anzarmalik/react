e67052e8cf8fe168ff956cf7c6de597c58025884
Anzar Malik <anzarmalikcse@gmail.com>
2023-07-22 13:42:58 +0530

    adding handleSubmit

--- twitch/streams/client/src/components/streams/StreamCreate.js
+++ twitch/streams/client/src/components/streams/StreamCreate.js
@@ -2,15 +2,26 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-    renderInput = (formProps) => {
-        console.log("🚀 ~ file: StreamCreate.js:6 ~ StreamCreate ~ props:", formProps)
-        return < input {...formProps.input} />;
+    renderInput = ({ input, label }) => {
+        console.log("🚀 ~ file: StreamCreate.js:6 ~ StreamCreate ~ props:", input)
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        );
     }
+
+    onSubmit = (formValues) => {
+        console.log("🚀 ~ file: StreamCreate.js:16 ~ StreamCreate ~ formValues:", formValues);
+    }
+
     render() {
         return (
-            <form>
-                <Field name='title' component={this.renderInput} />
-                <Field name='description' component={this.renderInput} />
+            <form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)} >
+                <Field name='title' component={this.renderInput} label="Enter Title" />
+                <Field name='description' component={this.renderInput} label="Enter Description" />
+                <button className=" ui button primary" >Submit</button>
             </form>
         )
     }
