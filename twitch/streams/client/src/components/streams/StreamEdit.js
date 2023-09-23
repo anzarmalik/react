e96e9df8041efb3a76f4b50b01e0dc9d47e45149
Anzar Malik <anzarmalikcse@gmail.com>
2023-09-23 16:09:16 +0530

    change in edit stream using fetchStream action

--- twitch/streams/client/src/components/streams/StreamEdit.js
+++ twitch/streams/client/src/components/streams/StreamEdit.js
@@ -1,15 +1,24 @@
 import React from "react";
 import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
 
 
-const StreamEdit =(props) => {
-    console.log("🚀 ~ file: StreamEdit.js:4 ~ StreamEdit ~ props:", props);
-    
-    return <div>StreamEdit</div>
+class StreamEdit extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id);
+    }
+
+    render() {
+        if (!this.props.stream) {
+            return <div>Loading</div>
+        }
+        return <div>{this.props.stream.description}</div>
+    }
 }
 
-const mapStateToProps = (state, ownProps)=>{
-return { stream: state.streams[ownProps.match.params.id] };
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
 }
 
-export default  connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps,{fetchStream})(StreamEdit);
