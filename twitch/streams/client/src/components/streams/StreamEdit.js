81aef7485efcf257ed422ae47744e09594d46607
Anzar Malik <anzarmalikcse@gmail.com>
2023-09-23 14:24:45 +0530

    working on edit stream data

--- twitch/streams/client/src/components/streams/StreamEdit.js
+++ twitch/streams/client/src/components/streams/StreamEdit.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { connect } from 'react-redux';
 
-const StreamEdit =()=>{
+
+const StreamEdit =(props) => {
+    console.log("🚀 ~ file: StreamEdit.js:4 ~ StreamEdit ~ props:", props);
+    
     return <div>StreamEdit</div>
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps)=>{
+return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default  connect(mapStateToProps)(StreamEdit);
